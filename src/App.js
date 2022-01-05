eab56b09fe356f86d66b86628b469dfc90e53035
pasilaaksonen <pasi.laaksonen@edu.turkuamk.fi>
2022-01-05 15:09:37 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,17 +2,23 @@ import React, { useState, useRef } from "react";
 import './App.css';
 import Headeri from './components/Headeri';
 import Kalenteri from './components/Kalenteri';
-import Listat from './components/Listat'
+import Lista from './components/Lista'
 import { useReactToPrint } from 'react-to-print';
+import LuoLista from "./components/LuoLista";
 
 
 function App() {
 
-  const [pdfLists, setPdfLists] = useState(null)
+  const [ pdfLists, setPdfLists ] = useState(null)
+  const [ printMode, setPrintMode ] = useState(false)
+  const [ startDate, setStartDate ] = useState(new Date());
   const componentRef = useRef();
+
   const handlePrint = () => {
+    setPrintMode(true)
     print()
   }
+
   const print = useReactToPrint({
     content: () => componentRef.current,
   });
@@ -21,12 +27,20 @@ function App() {
     <div className="App">
       <Headeri />
       <div className="page-content">
-        <Kalenteri setPdfLists={setPdfLists} handlePrint={handlePrint} />
         {
-          pdfLists && <Listat ref={componentRef} pdfLists={pdfLists} />
+          !pdfLists && <>
+                        <Kalenteri setStartDate={setStartDate} startDate={startDate} />
+                        <LuoLista setPdfLists={setPdfLists} handlePrint={handlePrint} startDate={startDate} />
+                       </>
         }
         {
-          pdfLists && <button onClick={handlePrint} className="print-button">Tulosta</button>
+          pdfLists && <Lista ref={componentRef} pdfLists={pdfLists} printMode={printMode} />
+        }
+        {
+          pdfLists && <>
+                        <button className="cancel-button" onClick={() => setPdfLists(null)}>Peruuta</button>
+                        <button onClick={handlePrint} className="print-button">Tulosta</button>
+                      </>
         } 
       </div>
     </div>
